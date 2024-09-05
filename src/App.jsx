569bf143d7a7896a3cbc61b1cb194d7b797fde8d
Utkarsh Mishra <136327443+utkarshethical27@users.noreply.github.com>
2024-09-05 15:18:39 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from './pages/home/home'
+import Services from './pages/services/services'
 import Posts from './pages/posts/posts'
 import About from './pages/about/about'
 import Contact from './pages/contact/contact'
@@ -12,6 +13,7 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
         <Route path="/posts/" element={<Posts />} />
         <Route path='/post/:id' element={<Post />} />
         <Route path="/about/" element={<About />} />
