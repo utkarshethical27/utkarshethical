983f1928176a1ddd0444e7a6f640e1d11b20efd3
Utkarsh Mishra <136327443+utkarshethical27@users.noreply.github.com>
2024-09-24 15:56:42 +0530

    Update utkarsh.jsx

--- src/pages/utkarsh/utkarsh.jsx
+++ src/pages/utkarsh/utkarsh.jsx
@@ -1,8 +1,8 @@
 import Nav from '../../components/nav/nav'
-import './home.css'
+import './utkarsh.css'
 import { Link } from 'react-router-dom';
 
-export default function Home() {
+export default function Utkarsh() {
     return (
         <>
             <Nav />
