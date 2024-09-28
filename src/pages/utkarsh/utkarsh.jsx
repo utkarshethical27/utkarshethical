a2f7ce694a412022ef71dd5f5632c564e9f8ea62
Utkarsh Mishra <136327443+utkarshethical27@users.noreply.github.com>
2024-09-28 14:57:01 +0530

    Update utkarsh.jsx

--- src/pages/utkarsh/utkarsh.jsx
+++ src/pages/utkarsh/utkarsh.jsx
@@ -12,7 +12,7 @@ export default function Utkarsh() {
             <div id="login">
                 <input type="text" placeholder="Enter username" id="user" value={user} onChange={(e)=>setUser(e.target.value)}/>
                 <input type="password" placeholder="Enter password" id="pass" value={pass} onChange={(e)=>setPass(e.target.value)}/>
-                <input type="button" value="Authenticate" id="submit" style={{opacity: (user.trim()!=='' && pass.trim()!=='')?1:.5}}</input>/>
+                <input type="button" value="Authenticate" id="submit" style={{opacity: (user.trim()!=='' && pass.trim()!=='')?1:.5}}/>
             </div>
         </>
     )
