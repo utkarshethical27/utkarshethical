3755a7d3b3af8c73b5741725f375ef22bafabe6b
Utkarsh Mishra <136327443+utkarshethical27@users.noreply.github.com>
2024-09-28 14:24:52 +0530

    Update utkarsh.jsx

--- src/pages/utkarsh/utkarsh.jsx
+++ src/pages/utkarsh/utkarsh.jsx
@@ -1,14 +1,17 @@
 import Nav from '../../components/nav/nav'
 import './utkarsh.css'
 import { Link } from 'react-router-dom';
+import {useState} from 'react'
 
 export default function Utkarsh() {
+    const [user, setUser] = useState('')
+    const [pass, setPass] = useState('')
     return (
         <>
             <Nav />
             <div id="login">
-                <input type="text" placeholder="Enter username" id="user"/>
-                <input type="password" placeholder="Enter password" id="pass"/>
+                <input type="text" placeholder="Enter username" id="user" value={user} onChange={(e)=>setUser(e)}/>
+                <input type="password" placeholder="Enter password" id="pass" value={pass} onChange={(e)=>setPass(e)}/>
                 <input type="button" value="Authenticate" id="submit"/>
             </div>
         </>
