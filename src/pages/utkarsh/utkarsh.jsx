3e667d12652d3cd2b4518eef37f7fdcf676cb51b
Utkarsh Mishra <136327443+utkarshethical27@users.noreply.github.com>
2024-09-30 06:27:18 +0530

    Update utkarsh.jsx

--- src/pages/utkarsh/utkarsh.jsx
+++ src/pages/utkarsh/utkarsh.jsx
@@ -19,7 +19,7 @@ export default function Utkarsh() {
                     setShowWel(true)
                     setTimeout(()=>{setShowWel(false)}, 1000)
                     const files = await axios.get('https://utkarshethicalserver.vercel.app/api/getFiles')
-                    alert(typeof files.data.resources)
+                    alert(typeof files.data.resource)
                 }else{
                     alert('Password Galat Hai')
                 }
